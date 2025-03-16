71c87892d0122843298e127b4b329be173920f84
RakeenAkiel <rakeen1012@gmail.com>
2025-03-16 12:14:15 +0100

    write function get_max_pages

--- scrape.py
+++ scrape.py
@@ -1,3 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+from string import ascii_lowercase
 
+def get_max_pages(alphabet, base_url):
+    page = 1
+    while True:
+        url = base_url.format(alphabet=alphabet, page=page)
+        response = requests.get(url)
+        if response.status_code != 200:
+            return page - 1 
+        page += 1
