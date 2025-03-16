0a220604a8a38e4107edfb830db7a627dd67a637
RakeenAkiel <rakeen1012@gmail.com>
2025-03-16 12:20:00 +0100

    write function extract_links

--- scrape.py
+++ scrape.py
@@ -10,3 +10,25 @@ def get_max_pages(alphabet, base_url):
         if response.status_code != 200:
             return page - 1 
         page += 1
+
+def extract_links(alphabet, base_url):
+    recipe_links = []
+    max_pages = get_max_pages(alphabet, base_url)
+
+    for page in range(1, max_pages + 1):
+        url = base_url.format(alphabet=alphabet, page=page)
+        respose = requests.get(url)
+
+        if response.status_code != 200:
+            break # stop if the page does not exist
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        # extract recipe links
+        for div in soup.find_all("div", class_="gel-layout__item"):
+            a_tag = div.find("a", class_="promo")
+            if a_tag and "href" in a_tag.attrs:
+                link = "https://www.bbc.co.uk" + a_tag["href"]
+                recipe_links.append(link)
+    
+    return recipe_links
