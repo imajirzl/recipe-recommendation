d0fae17abec5aa52cd5397c5748693a4ccc1cbb2
RakeenAkiel <rakeen1012@gmail.com>
2025-03-17 15:28:56 +0100

    adjust extract_link for pagination with disabled next button

--- web_scraping/scrape.py
+++ web_scraping/scrape.py
@@ -2,22 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 from string import ascii_lowercase
 
-#def get_max_pages(alphabet, base_url):
-#    page = 1
-#    while True:
-#        url = base_url.format(alphabet=alphabet, page=page)
-#        response = requests.get(url)
-#        if response.status_code != 200:
-#            return min(page - 1, 15) # take only up to 15 pages 
-#        page += 1
-#    return page
-
-def extract_links(alphabet, base_url, max_pages=15):
+def extract_links(alphabet, base_url):
     recipe_links = []
-    #max_pages = get_max_pages(alphabet, base_url)
-    #print(f"{alphabet.upper()}: max pages - {max_pages}")
-
-    for page in range(1, max_pages + 1):
+    page = 1
+    
+    while True:
         url = base_url.format(alphabet=alphabet, page=page)
         response = requests.get(url)
 
@@ -32,9 +21,15 @@ def extract_links(alphabet, base_url, max_pages=15):
             if a_tag and "href" in a_tag.attrs:
                 link = "https://www.bbc.co.uk" + a_tag["href"]
                 recipe_links.append(link)
+
+        next_button = soup.find("span", class_="pagination__link--disabled")
+        if next_button:
+            break
+
+        page += 1
                 
     print(f"Extracted {len(recipe_links)} links for {alphabet.upper()}")
-    
+
     return recipe_links
 
 def extract_recipe_details(url):
