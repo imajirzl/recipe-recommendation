4165ede21f8f5bd325104e77dcd0da39d0e14010
RakeenAkiel <rakeen1012@gmail.com>
2025-03-17 16:30:09 +0100

    adjust extract_links for pagination and stopping with disabled next button and running the module under test_module.ipynb

--- web_scraping/scrape.py
+++ web_scraping/scrape.py
@@ -18,17 +18,24 @@ def extract_links(alphabet, base_url):
         # extract recipe links
         for div in soup.find_all("div", class_="gel-layout__item"):
             a_tag = div.find("a", class_="promo")
-            if a_tag and "href" in a_tag.attrs:
+            if a_tag and "href" in a_tag.attrs and "/food/recipes/" in a_tag["href"]:
                 link = "https://www.bbc.co.uk" + a_tag["href"]
                 recipe_links.append(link)
-
+        
         pagination_div = soup.find("div", class_="pagination gel-wrap")
-        if pagination_div:
-            next_button_disabled = pagination_div.find("span", class_="pagination__link--disabled")
-            if next_button_disabled:
-                break
+        
+        # if no pagination exists, it's a single page result
+        if not pagination_div:
+            break
+
+        next_button_disabled = pagination_div.find("span", class_="pagination__link pagination__link--disabled gel-pica-bold", string=lambda text: text and "Next" in text)
+        
+        # if 'next' button is disabled, we're on the last page
+        if next_button_disabled:
+            break
 
         page += 1
+        first_page = False # after first page, no longer skip the check
                 
     print(f"Extracted {len(recipe_links)} links for {alphabet.upper()}")
 
